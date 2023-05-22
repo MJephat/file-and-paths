43f7b0356bab3d61ba0354ee34488e93f1bbf4e7
MJephat <mukundijephat22@gmail.com>
2023-05-22 20:46:31 +0300

    done

--- Ques 4/students.py
+++ Ques 4/students.py
@@ -5,9 +5,12 @@ with open('/home/jephat/desktop/phase-3/file-and-paths/Ques 4/student.csv') as f
     for row in reader:
         print(row)
 
-with open (' .') as file:
-    #pass file to csv reader(It creates an object reader)
+with open('/home/jephat/desktop/phase-3/file-and-paths/Ques 4/student.csv') as file:
     reader = csv.reader(file)
     for row in reader:
-       print(row)
+        if row[1] < '18':
+            with open ('/home/jephat/desktop/phase-3/file-and-paths/Ques 4/young_student.txt','a') as student_file:
+                student_file.write(str(row)+ '\n')
+        else:
+            print
 
